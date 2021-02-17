6707382d5e1c278d21d287308063d586c3182407
mincTeja <himateja135@gmail.com>
2021-02-17 09:34:36 +0530

    encoders and decoders done

---
+++ encoder.js
@@ -0,0 +1,21 @@
+function encodeURL(urlToBeEncoded){
+    return encodeURIComponent(urlToBeEncoded);
+  }
+
+  function decodeURL(urlToBeEncoded){
+    return decodeURIComponent(urlToBeEncoded);
+  }
+
+window.addEventListener('load',() => {
+    const encode = document.querySelector(".encode");
+    const decode = document.querySelector(".decode");
+    const inputText = document.querySelector(".text-input");
+    const outputText = document.querySelector(".text-output");
+    encode.addEventListener('click',()=>{
+        outputText.value = encodeURL(inputText.value);
+    });
+    decode.addEventListener('click',()=>{
+        outputText.value = decodeURL(inputText.value);
+    });
+});
+
